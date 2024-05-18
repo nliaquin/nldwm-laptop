a9af92ac8580ef81e7ce529d77993151e8cc3a6e
nliaquin <nickolasiaquinta@gmail.com>
2024-05-18 19:23:28 -0400

    Modified blocks.def.h and config.def.h

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+    /*Icon*/    /*Command*/    /*Update Interval*/    /*Update Signal*/
+    {"", "sh -c 'output=$(progress -q); if [ \"$output\" != \"No command currently running\" ]; then echo \"$output\" | head -n 2 | paste -sd \" \"; else echo \"\"; fi'", 1, 12},
+    {"RAM:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 1, 0},
+    {"BRI:", "echo $((100 * $(brightnessctl get) / $(brightnessctl max)))%", 1, 11},
+    {"BAT:", "cat /sys/class/power_supply/BAT0/capacity", 30, 0},
+    {"", "cat /sys/class/power_supply/BAT0/status", 5, 0},
+    {"", "date '+%D %R'", 5, 0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
